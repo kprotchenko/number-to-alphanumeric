8abf7186cb51cef0c0396b48e4a6ef6306e1b1b5
Kyrylo Protchenko <kprotchenko@gmail.com>
2020-05-13 23:42:01 -0400

    updating method names in controller class

--- server/src/main/java/com/pk/numberparser/controllers/PhoneNumberController.java
+++ server/src/main/java/com/pk/numberparser/controllers/PhoneNumberController.java
@@ -25,66 +25,24 @@ class PhoneNumberController {
 
     @GetMapping("/get-phone-numbers")
     @CrossOrigin(origins = "http://localhost:4200")
-    public Collection<Number> phoneNumbers() {
-        return phoneNumberRepository.findAll().stream()
+    public Collection<Number> getAllSavedPhoneNumbers() {
+        return phoneNumberRepository.findAll()
+                .stream()
                 .collect(Collectors.toList());
     }
+
     @RequestMapping(value = "/get-real-number/{alphaNumeric}", method = RequestMethod.GET)
     @CrossOrigin(origins = "http://localhost:4200")
-    public String realNumber(@PathVariable String alphaNumeric) {
+    public String convertAlphanumericToRealNumber(@PathVariable String alphaNumeric) {
         StopWatch stopWatch = new StopWatch("getNumericCombination");
         stopWatch.start();
         try {
-
             String numeric = phoneNumberService.numberCombination(alphaNumeric);
             return numeric;
         } finally {
             stopWatch.stop();
             stopWatch.prettyPrint();
         }
-    }
-
-    @RequestMapping(value = "/get-number/{id}", method = RequestMethod.GET)
-    @CrossOrigin(origins = "http://localhost:4200")
-    public Number number(@PathVariable Long id) {
-        return getNumber(id);
-    }
-
-    private Number getNumber(@PathVariable Long id) {
-        Collection<Long> singleIdList = new ArrayList<>();
-        singleIdList.add(id);
-        return phoneNumberRepository.findAllById(singleIdList).stream().peek(number -> {
-            StopWatch stopWatch = new StopWatch("getNumericCombination");
-            Set<String> letterCombinations = new HashSet<>();
-//            String numeric = number.getNumber();
-//            if (StringUtils.isEmpty(numeric)) {
-//                numeric = phoneNumberService.numberCombination(number.getName());
-//                number.setNumber(numeric);
-//            }
-//            phoneNumberRepository.save(number);
-            try {
-                stopWatch.start("conversion_for_number --> "+number.getName());
-                letterCombinations = phoneNumberService.letterCombinations(number.getNumber());
-            } finally {
-                stopWatch.stop();
-                number.setStopWatchReportOnLetterCombination(stopWatch.prettyPrint());
-            }
-            number.setLetterCombinationsString(letterCombinations.toString());
-            number.setLetterCombinations(letterCombinations);
-            number.setLetterCombinationsSize(Long.valueOf(letterCombinations.size()));
-        }).findFirst().get();
-    }
-
-    @RequestMapping(value = "/save-number", method = RequestMethod.POST)
-    @CrossOrigin(origins = "http://localhost:4200")
-    public Number saveNumber(@RequestBody Number number) {
-//        StopWatch stopWatch = new StopWatch("getAlphanumericCombinations");
-//        String numeric;
-//        numeric = phoneNumberService.numberCombination(number.getName());
-//        number.setNumber(numeric);
-//        phoneNumberRepository.save(number);
-        Number numberResult = phoneNumberRepository.save(number);
-        return getNumber(numberResult.getId());
     }
 
     @RequestMapping(value = "/get-letter-combinations/{phoneNumber}", method = RequestMethod.GET)
@@ -99,6 +57,40 @@ class PhoneNumberController {
             stopWatch.prettyPrint();;
         }
     }
+
+    @RequestMapping(value = "/get-number/{id}", method = RequestMethod.GET)
+    @CrossOrigin(origins = "http://localhost:4200")
+    public Number getNumberById(@PathVariable Long id) {
+        return getNumber(id);
+    }
+
+    @RequestMapping(value = "/save-number", method = RequestMethod.POST)
+    @CrossOrigin(origins = "http://localhost:4200")
+    public Number saveNumber(@RequestBody Number number) {
+        Number numberResult = phoneNumberRepository.save(number);
+        return getNumber(numberResult.getId());
+    }
+
+    private Number getNumber(@PathVariable Long id) {
+        Collection<Long> singleIdList = new ArrayList<>();
+        singleIdList.add(id);
+        return phoneNumberRepository.findAllById(singleIdList).stream().peek(number -> {
+            StopWatch stopWatch = new StopWatch("getNumericCombination");
+            Set<String> letterCombinations = new HashSet<>();
+            try {
+                stopWatch.start("conversion_for_number --> "+number.getName());
+                letterCombinations = phoneNumberService.letterCombinations(number.getNumber());
+            } finally {
+                stopWatch.stop();
+                number.setStopWatchReportOnLetterCombination(stopWatch.prettyPrint());
+            }
+            number.setLetterCombinationsString(letterCombinations.toString());
+            number.setLetterCombinations(letterCombinations);
+            number.setLetterCombinationsSize(Long.valueOf(letterCombinations.size()));
+        }).findFirst().get();
+    }
+
+
 
 
 }
