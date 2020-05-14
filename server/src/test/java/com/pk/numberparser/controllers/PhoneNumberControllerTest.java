2064cde01f13fe000389f2d9d55fe2a79ad83821
Kyrylo Protchenko <kprotchenko@gmail.com>
2020-05-13 23:43:30 -0400

    adding test stubs

---
+++ server/src/test/java/com/pk/numberparser/controllers/PhoneNumberControllerTest.java
@@ -0,0 +1,28 @@
+package com.pk.numberparser.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PhoneNumberControllerTest {
+
+    @Test
+    void getAllSavedPhoneNumbers() {
+    }
+
+    @Test
+    void convertAlphanumericToRealNumber() {
+    }
+
+    @Test
+    void getLetterCombinationsForANumber() {
+    }
+
+    @Test
+    void getNumberById() {
+    }
+
+    @Test
+    void saveNumber() {
+    }
+}
---
+++ server/src/test/java/com/pk/numberparser/enteties/NumberTest.java
@@ -0,0 +1,64 @@
+package com.pk.numberparser.enteties;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getNumber() {
+    }
+
+    @Test
+    void setNumber() {
+    }
+
+    @Test
+    void getLetterCombinations() {
+    }
+
+    @Test
+    void setLetterCombinations() {
+    }
+
+    @Test
+    void getLetterCombinationsString() {
+    }
+
+    @Test
+    void setLetterCombinationsString() {
+    }
+
+    @Test
+    void getLetterCombinationsSize() {
+    }
+
+    @Test
+    void setLetterCombinationsSize() {
+    }
+
+    @Test
+    void getStopWatchReportOnLetterCombination() {
+    }
+
+    @Test
+    void setStopWatchReportOnLetterCombination() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+}
