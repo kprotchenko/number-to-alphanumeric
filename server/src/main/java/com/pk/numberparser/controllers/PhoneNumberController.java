5610970ad898df2c19aacb902d6e3ceb72818f2f
Kyrylo Protchenko <kprotchenko@gmail.com>
2020-05-18 00:00:14 -0400

    adding tests for controller

--- server/src/main/java/com/pk/numberparser/controllers/PhoneNumberController.java
+++ server/src/main/java/com/pk/numberparser/controllers/PhoneNumberController.java
@@ -5,92 +5,97 @@ import com.pk.numberparser.service.PhoneNumberService;
 import com.pk.numberparser.service.StopWatch;
 import com.pk.numberparser.enteties.Number;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RestController
 class PhoneNumberController {
     private PhoneNumberRepository phoneNumberRepository;
-    @Autowired
     private PhoneNumberService phoneNumberService;
 
-    public PhoneNumberController(PhoneNumberRepository phoneNumberRepository) {
+    public PhoneNumberController(PhoneNumberRepository phoneNumberRepository, PhoneNumberService phoneNumberService) {
         this.phoneNumberRepository = phoneNumberRepository;
+        this.phoneNumberService = phoneNumberService;
     }
 
     @GetMapping("/get-phone-numbers")
     @CrossOrigin(origins = "http://localhost:4200")
-    public Collection<Number> getAllSavedPhoneNumbers() {
+    public @ResponseBody Collection<Number> getAllSavedPhoneNumbers() {
         return phoneNumberRepository.findAll()
                 .stream()
                 .collect(Collectors.toList());
     }
 
-    @RequestMapping(value = "/get-real-number/{alphaNumeric}", method = RequestMethod.GET)
+    @GetMapping(value = "/get-real-number/{alphaNumeric}")
     @CrossOrigin(origins = "http://localhost:4200")
-    public String convertAlphanumericToRealNumber(@PathVariable String alphaNumeric) {
+    public @ResponseBody String convertAlphanumericToRealNumber(@PathVariable String alphaNumeric) {
         StopWatch stopWatch = new StopWatch("getNumericCombination");
         stopWatch.start();
         try {
-            String numeric = phoneNumberService.numberCombination(alphaNumeric);
-            return numeric;
+            return phoneNumberService.numberCombination(alphaNumeric);
         } finally {
             stopWatch.stop();
             stopWatch.prettyPrint();
         }
     }
 
-    @RequestMapping(value = "/get-letter-combinations/{phoneNumber}", method = RequestMethod.GET)
+    @GetMapping(value = "/get-letter-combinations/{phoneNumber}")
     @CrossOrigin(origins = "http://localhost:4200")
-    public Collection<String> getLetterCombinationsForANumber(@PathVariable String phoneNumber) {
+    public @ResponseBody Collection<String> getLetterCombinationsForANumber(@PathVariable String phoneNumber) {
         StopWatch stopWatch = new StopWatch("getAlphanumericCombinations");
         stopWatch.start();
         try {
             return phoneNumberService.letterCombinations(phoneNumber);
         } finally {
             stopWatch.stop();
-            stopWatch.prettyPrint();;
+            stopWatch.prettyPrint();
         }
     }
 
-    @RequestMapping(value = "/get-number/{id}", method = RequestMethod.GET)
+    @GetMapping(value = "/get-number/{id}")
     @CrossOrigin(origins = "http://localhost:4200")
-    public Number getNumberById(@PathVariable Long id) {
-        return getNumber(id);
+    public ResponseEntity<Number> getNumberById(@PathVariable Long id) {
+        return getNumber(id)
+                .map(existing-> ResponseEntity.ok().body(existing))
+                .orElse(ResponseEntity.notFound().build());
     }
 
-    @RequestMapping(value = "/save-number", method = RequestMethod.POST)
+    @PostMapping(value = "/save-number")
     @CrossOrigin(origins = "http://localhost:4200")
-    public Number saveNumber(@RequestBody Number number) {
+    public ResponseEntity<Number> saveNumber(@RequestBody Number number) {
         Number numberResult = phoneNumberRepository.save(number);
-        return getNumber(numberResult.getId());
+        return getNumber(numberResult.getId())
+                .map(existing-> ResponseEntity.status(HttpStatus.CREATED).body(existing))
+                .orElse(ResponseEntity.notFound().build());
     }
 
-    private Number getNumber(@PathVariable Long id) {
+    private Optional<Number> getNumber(@PathVariable Long id) {
         Collection<Long> singleIdList = new ArrayList<>();
         singleIdList.add(id);
-        return phoneNumberRepository.findAllById(singleIdList).stream().peek(number -> {
-            StopWatch stopWatch = new StopWatch("getNumericCombination");
-            Set<String> letterCombinations = new HashSet<>();
-            try {
-                stopWatch.start("conversion_for_number --> "+number.getName());
-                letterCombinations = phoneNumberService.letterCombinations(number.getNumber());
-            } finally {
-                stopWatch.stop();
-                number.setStopWatchReportOnLetterCombination(stopWatch.prettyPrint());
-            }
-            number.setLetterCombinationsString(letterCombinations.toString());
-            number.setLetterCombinations(letterCombinations);
-            number.setLetterCombinationsSize(Long.valueOf(letterCombinations.size()));
-        }).findFirst().get();
+        return phoneNumberRepository.findAllById(singleIdList)
+                .stream()
+                .map(this::addAlphanumerics)
+                .findFirst().get();
     }
 
 
-
-
+    private Optional<Number> addAlphanumerics(Number number) {
+        StopWatch stopWatch = new StopWatch("getNumericCombination");
+        Set<String> letterCombinations = new HashSet<>();
+        try {
+            stopWatch.start("conversion_for_number --> " + number.getName());
+            letterCombinations = phoneNumberService.letterCombinations(number.getNumber());
+        } finally {
+            stopWatch.stop();
+            number.setStopWatchReportOnLetterCombination(stopWatch.prettyPrint());
+        }
+        number.setLetterCombinationsString(letterCombinations.toString());
+        number.setLetterCombinations(letterCombinations);
+        number.setLetterCombinationsSize(Long.valueOf(letterCombinations.size()));
+        return Optional.ofNullable(number);
+    }
 }
--- server/src/test/java/com/pk/numberparser/controllers/PhoneNumberControllerTest.java
+++ server/src/test/java/com/pk/numberparser/controllers/PhoneNumberControllerTest.java
@@ -1,28 +1,139 @@
 package com.pk.numberparser.controllers;
 
+import com.pk.numberparser.enteties.Number;
+import com.pk.numberparser.repositories.PhoneNumberRepository;
+import com.pk.numberparser.service.PhoneNumberService;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class PhoneNumberControllerTest {
+
+    @Mock
+    private PhoneNumberService phoneNumberService;
+
+    @Mock
+    private PhoneNumberRepository phoneNumberRepository;
+
+    @InjectMocks
+    private PhoneNumberController phoneNumberController;
 
     @Test
     void getAllSavedPhoneNumbers() {
+        Number number = createNumberOperater(Long.valueOf(111));
+        final Collection<Number> output = new ArrayList<>();
+        output.add(number);
+        when(phoneNumberController.getAllSavedPhoneNumbers()).thenReturn(output);
+        final Collection<Number> result = phoneNumberController.getAllSavedPhoneNumbers();
+        assertArrayEquals(output.toArray(),result.toArray());
     }
 
     @Test
     void convertAlphanumericToRealNumber() {
+        String alphanumeric = "1-800-OPERATER";
+        String output = "18006737283";
+        when(phoneNumberService.numberCombination(alphanumeric)).thenReturn(output);
+        final String result = phoneNumberController.convertAlphanumericToRealNumber(alphanumeric);
+        assertEquals(output,result);
     }
 
     @Test
     void getLetterCombinationsForANumber() {
+        String numeric = "22";
+        Set<String> output = getLetterCombinationsSet();
+        when(phoneNumberService.letterCombinations(numeric)).thenReturn(output);
+        final Collection<String> result = phoneNumberController.getLetterCombinationsForANumber(numeric);
+        assertArrayEquals(output.toArray(),result.toArray());
     }
+
+
 
     @Test
     void getNumberById() {
+        final Number output = prepOutputForUpdateOrSave();
+        final ResponseEntity<Number> result = phoneNumberController.getNumberById(Long.valueOf(22));
+        assertEquals(HttpStatus.OK,result.getStatusCode());
+        checkGetNumberAssertions(output, result);
     }
 
     @Test
     void saveNumber() {
+        final Number input = createNumber(Long.valueOf(22));
+        final Number output = prepOutputForUpdateOrSave();
+        when(phoneNumberRepository.save(input)).thenReturn(input);
+        final ResponseEntity<Number> result = phoneNumberController.saveNumber(input);
+        assertEquals(HttpStatus.CREATED,result.getStatusCode());
+        checkGetNumberAssertions(output, result);
     }
+
+    private Number prepOutputForUpdateOrSave() {
+        final Number output = createNumber(Long.valueOf(22));
+        final Collection<Number> outputList = new ArrayList<>();
+        outputList.add(output);
+        Collection<Long> singleIdList = new ArrayList<>();
+        singleIdList.add(output.getId());
+        when(phoneNumberRepository.findAllById(singleIdList)).thenReturn((List<Number>) outputList);
+        when(phoneNumberService.letterCombinations(output.getNumber())).thenReturn(getLetterCombinationsSet());
+        output.setLetterCombinations(getLetterCombinationsSet());
+        output.setLetterCombinationsString(output.getLetterCombinations().toString());
+        return output;
+    }
+
+    private void checkGetNumberAssertions(Number output, ResponseEntity<Number> result) {
+        assertNotNull(result.getBody());
+        assertNotNull(result.getBody().getStopWatchReportOnLetterCombination());
+        assertEquals(output.getId(), result.getBody().getId());
+        assertEquals(output.getNumber(), result.getBody().getNumber());
+        assertNotNull(result.getBody().getLetterCombinations());
+        assertArrayEquals(output.getLetterCombinations().toArray(), result.getBody().getLetterCombinations().toArray());
+    }
+
+    private Number createNumberOperater(Long id) {
+        Number number = new Number();
+        number.setId(id);
+        number.setName(String.format("1-%s-OPERATER",id.toString()));
+        number.setNumber(String.format("1-%s-673-7283",id.toString()));
+        return number;
+    }
+    private Number createNumber(Long id) {
+        Number number = new Number();
+        number.setId(id);
+        number.setName(id.toString());
+        number.setNumber(id.toString());
+        return number;
+    }
+
+    private Set<String> getLetterCombinationsSet() {
+        Set<String> output = new HashSet<>();
+        output.add("22");
+        output.add("aa");
+        output.add("bb");
+        output.add("cc");
+        output.add("ab");
+        output.add("bc");
+        output.add("ac");
+        output.add("2a");
+        output.add("c2");
+        output.add("2b");
+        output.add("b2");
+        output.add("2c");
+        output.add("a2");
+        output.add("ca");
+        output.add("ba");
+        output.add("cb");
+        return output;
+    }
+
 }
